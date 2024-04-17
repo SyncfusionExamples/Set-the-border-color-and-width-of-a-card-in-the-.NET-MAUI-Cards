10ed9766064e1ba7dae1e7bac4fa7801076af2ac
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2024-04-17 09:23:05 +0530

    KB sample added

---
+++ CardsBorderCustomization/CardsBorderCustomization/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CardsBorderCustomization
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CardsBorderCustomization/CardsBorderCustomization/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CardsBorderCustomization
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
